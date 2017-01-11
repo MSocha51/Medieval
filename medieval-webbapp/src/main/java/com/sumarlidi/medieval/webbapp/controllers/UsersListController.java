3a69d32fde4959a1716f37f88f61d93a737300af
MSocha51 <m.socha51@wp.pl>
2017-01-11 15:07:10 +0100

    SNAPSHOT
    Start work on admin panel

---
+++ medieval-webbapp/src/main/java/com/sumarlidi/medieval/webbapp/controllers/UsersListController.java
@@ -0,0 +1,13 @@
+package com.sumarlidi.medieval.webbapp.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+public class UsersListController extends PageController {
+	
+	@RequestMapping("/users")
+	public String getUsers(Model model){
+		return "users-list";
+	}
+}
